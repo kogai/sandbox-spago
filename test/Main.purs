641d97e62c514f0026897884847541a21302aaa2
Shinichi Kogai <kogai0121@gmail.com>
2019-03-14 13:00:19 +0900

    add dep

--- test/Main.purs
+++ test/Main.purs
@@ -7,7 +7,11 @@ import Effect.Console (log)
 
 import Main as Main
 
+id :: ∀ a. a -> a
+id x = x
+
 main :: Effect Unit
 main = do
   Main.main
-  log "You should add some tests."
+  let ten = show $ id 10
+  log $ "You should add some tests." <> ten
